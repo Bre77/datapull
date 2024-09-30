fab5b44df212360fc569dc8ea3e6fa9a140c9755
Brett <brett.whynot@gmail.com>
2024-09-30 19:56:23 +1000

    Add build

---
+++ .build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd "${0%/*}"
+OUTPUT="${1:-datapull.spl}"
+chmod -R u=rwX,go= *
+chmod -R u-x+X *
+chmod -R u=rwx,go= bin/*
+python3.9 -m pip install --upgrade -t lib -r lib/requirements.txt --no-dependencies
+rm -rf lib/*/__pycache__
+rm -rf lib/*/*/__pycache__
+cd ..
+tar -cpzf $OUTPUT --exclude=.* --overwrite datapull 
